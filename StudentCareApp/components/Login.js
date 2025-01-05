46785f6f1766b21ed95110728f4ecd31dc5beabb
Layuru Karunathilaka <139636618+LayuruLK@users.noreply.github.com>
2025-01-05 18:40:10 +0000

    Handle student login details

--- StudentCareApp/components/Login.js
+++ StudentCareApp/components/Login.js
@@ -1,5 +1,26 @@
 import { Image, ScrollView, StyleSheet, View } from "react-native";
+import { Button, Divider, PaperProvider, Text, TextInput } from "react-native-paper";
+import { students } from "../data/StudentsDb";
+import { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
 export default function Login() {
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigation();
+
+    const handlebutton = () => {
+        const student = students.find(
+          (student) => student.username === name && student.password === password
+        );
+    
+        if (student) {
+          navigate.navigate("profile", { studentdata: student });
+        } else {
+          alert("Invalid username or password.");
+        }
+      };
+    
+
     return(
         <PaperProvider>
           <ScrollView>
