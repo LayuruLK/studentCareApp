730996e3fcb4ded35984fb13d17fb8046936bdec
Layuru Karunathilaka <139636618+LayuruLK@users.noreply.github.com>
2025-01-05 18:49:35 +0000

    Added texts and images

---
+++ StudentCareApp/components/Profile.js
@@ -0,0 +1,17 @@
+export default function Profile() {
+    return(
+        <PaperProvider>
+           <ScrollView>
+           <View style={styles.header}>
+              <Text style={styles.headerText}>UoV Student Care</Text>
+           </View>
+           <View style={styles.imagecontain}>
+             <Image
+               source={require("../assets/uovlogo.png")}
+               style={styles.image}
+             />
+           </View> 
+         </ScrollView> 
+      </PaperProvider>
+    );
+}
