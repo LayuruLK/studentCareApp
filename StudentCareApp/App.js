dd4060c57cd5f78ea9408d5d6a1b4fd35cbd6f32
Layuru Karunathilaka <139636618+LayuruLK@users.noreply.github.com>
2025-01-05 19:31:34 +0000

    Added styles

--- StudentCareApp/App.js
+++ StudentCareApp/App.js
@@ -30,8 +30,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
   },
 });
+
