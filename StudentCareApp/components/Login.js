8f51feb7105f740f24e6c748a9dbfe2e371cd1ad
Layuru Karunathilaka <139636618+LayuruLK@users.noreply.github.com>
2025-01-05 18:34:29 +0000

    Added header for login

---
+++ StudentCareApp/components/Login.js
@@ -0,0 +1,11 @@
+export default function Login() {
+    return(
+        <PaperProvider>
+          <ScrollView>
+          <View style={styles.header}>
+          <Text style={styles.headerText}>UoV Student Care</Text>
+        </View>
+          </ScrollView>  
+        </PaperProvider>
+    );
+}
